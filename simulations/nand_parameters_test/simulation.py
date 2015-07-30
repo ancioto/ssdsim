d492291ee1ec8f47ff549f6d3e44858e54ea7076
Nicholas Fiorentini <nicholas@comidoo.com>
2015-07-30 19:16:09 +0200

    Simulation nand sizes

--- simulations/nand_parameters_test/simulation.py
+++ simulations/nand_parameters_test/simulation.py
@@ -16,20 +16,23 @@ from simulator.NAND.NANDFactory import get_instance, GARBAGECOLLECTOR_SIMPLE
 def main():
     # create the simulation
     demo = Simulation(simulation_name="nand_parameters_test",
-                      sample_size=100000, sampling=10000,
-                      sampling_type=SIM_SAMPLING_HOST_WRITE)
+                      sample_size=10 ** 5, sampling_type=SIM_SAMPLING_HOST_WRITE)
     demo.init_simulation(base_path="./simulations/RESULTS/")
 
     # create various disks with different gc parameters
     for name, blocks, pages in (
             # the size is kept constant (128 MiB)
-            ("256x128", 256, 128),  # original parameters (128 MiB)
+            ("2x16384", 2, 16384),  # 128 MiB
+            ("16x2048", 16, 2048),  # 128 MiB
+            ("32x1024", 32, 1024),  # 128 MiB
             ("64x512", 64, 512),  # 128 MiB
             ("128x256", 128, 256),  # 128 MiB
+            ("256x128", 256, 128),  # original parameters (128 MiB)
             ("512x64", 512, 64),  # 128 MiB
-            ("1024x32", 1024, 32),  # 128 MiB
-            ("2048x16", 2048, 16),  # 128 MiB
-            ("4096x8", 4096, 8)):  # 128 MiB
+            ("1024x32", 1024, 32)):  # 128 MiB
+            # ("2048x16", 2048, 16)):  # 128 MiB Simulation too expensive (anyway no improvements in results)
+            # ("4096x8", 4096, 8),  # 128 MiB Simulation too expensive (anyway no improvements in results)
+            # ("16384x2", 16384, 2),  # 128 MiB Simulation too expensive (anyway no improvements in results)
         demo.add_disk(name, get_instance(garbagecollector=GARBAGECOLLECTOR_SIMPLE,
                                          total_blocks=blocks, pages_per_block=pages))
 
